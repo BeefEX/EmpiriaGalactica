52b7b251b3183ba1d8452626aace8cc07763b336
Jan Polák <janpolak.lysa@gmail.com>
2018-04-06 11:35:19 +0200

    Started work on a GUI version.

---
+++ EmpiriaGalactica-GUI/EmpiriaGalactica.cs
@@ -0,0 +1,21 @@
+﻿namespace EmpiriaGalactica_GUI {
+    
+    /// <summary>
+    /// The entry call of the game.
+    /// </summary>
+    internal static class EmpiriaGalactica {
+
+        /// <summary>
+        /// An window instance.
+        /// </summary>
+        private static Window _window;
+
+        
+        private static void Main(string[] args) {
+            global::EmpiriaGalactica.Managers.DependecyManager.Init();
+            
+            _window = new Window();
+            _window.Start();
+        }
+    }
+}
---
+++ EmpiriaGalactica-GUI/Window.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ImGuiNET;
+using ImGuiOpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+namespace EmpiriaGalactica_GUI {
+    
+    public class Window : ImGuiOpenTKWindow {
+        
+        public Window() : base("EmpiriaGalactica") {
+            
+        }
+
+        public override void ImGuiLayout() {
+            ImGui.BeginWindow("test");
+            ImGui.Text("this is a text");
+            ImGui.Image();
+            ImGui.EndWindow();
+        }
+    }
+}
